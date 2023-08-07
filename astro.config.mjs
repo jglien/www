a20e8e9793868bf890184b9d33115d3f825e8f6d
John Lien <johnglien@gmail.com>
2023-08-06 18:12:07 -0600

    Remove problematic dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import aws from "astro-sst/lambda";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: aws(),
-  integrations: [tailwind(), image(), compress(), sitemap()],
+  integrations: [tailwind(), image(), sitemap()],
 });
