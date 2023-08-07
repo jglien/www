b8118be9a2b3e57ed4d43fc14b15e9752e56baaa
John Lien <johnglien@gmail.com>
2023-08-06 17:16:58 -0700

    Initial Site (#1)
    * update gitignore
    
    * Initial site
    
    * oops
    
    * oops 2
    
    * better name
    
    * oops
    
    * Remove problematic dep
    
    * fix

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import aws from "astro-sst/lambda";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: aws(),
+  integrations: [tailwind(), image(), sitemap()],
+});
---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+export interface Contact {
+  link: string;
+  title: string;
+};
+
+export const contact: Contact[] = [
+  {
+    link: "mailto:john@johnlien.me",
+    title: "john@johnlien.me",
+  },
+  {
+    link: "https://www.linkedin.com/in/john-g-lien",
+    title: "LinkedIn",
+  },
+  {
+    link: "https://github.com/jglien",
+    title: "GitHub",
+  },
+]
