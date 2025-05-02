0b4ac294e344fa6f4662f5f50d952b0ac04e175c
John Lien <john@canopyservicing.com>
2025-05-01 22:34:20 -0600

    update everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import aws from "astro-sst";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: aws(),
-  integrations: [tailwind(), image(), sitemap()],
+  integrations: [tailwind(), sitemap()],
 });
